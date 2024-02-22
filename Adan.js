40a858e30392d1a434d32ad786e6715d64a4a3ab
Jesus <adan10104334@gmail.com>
2024-02-21 20:59:57 -0600

    Added my test

---
+++ Adan.js
@@ -0,0 +1,8 @@
+console.log("Hola mundo sumador y de pruebas jeje")
+
+//Script para sumar dos números
+function checkImageExtension(n1, n2){
+    return n1+n2;
+}
+
+module.exports = checkImageExtension
---
+++ __test__/Adan.test.js
@@ -0,0 +1,5 @@
+const checkSumExpression = require('../Adan')
+
+test('Si sabe sumar jeje', ()=>{
+    expect(checkSumExpression(10,5)).toBe(15)
+})
