85c68e8ea6523a95a5d75e31bd2e06e654fda033
unknown <adan10104334@gmail.com>
2024-03-20 14:54:40 -0600

    Fixed issues with Next caché

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,6 +1,9 @@
 import { MongoClient } from 'mongodb';
 import { NextResponse } from 'next/server';
 
+export const dynamic = "force-dynamic";
+export const revalidate = 1; // this is the new line added
+
 export async function GET () {
     const uri = process.env.MONGO_URL; // Reemplaza <username> y <password> con tus credenciales
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
--- src/app/page.js
+++ src/app/page.js
@@ -28,7 +28,7 @@ export default function Home() {
     };
 
     const getUsers = async () => {
-        const response = await fetch('https://social.babyday.studio/api/users');
+        const response = await fetch('https://social.babyday.studio/api/users', {cache: 'no-store'});
         const data = await response.json();
         return data; // Devuelve los datos en lugar de intentar cambiar el estado directamente
     }
